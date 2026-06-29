186b109f85b6a10e251b0ebc9809b0bcb7c99afa
iceboks <modpersys420@gmail.com>
2026-06-28 20:59:05 -0600

    Add Umami analytics tracking to site pages
    Load self-hosted Umami script from umami.americannex.com for iceboks.site
    visit and pageview metrics.

---
+++ site.assets/analytics.js
@@ -0,0 +1,7 @@
+(function () {
+  var s = document.createElement('script');
+  s.defer = true;
+  s.src = 'https://umami.americannex.com/script.js';
+  s.dataset.websiteId = 'e830235f-bbbc-45c1-a315-f09074e9a93b';
+  document.head.appendChild(s);
+})();
